7667b1cb8aa70a38f1667ded82002d52f2d7fd7c
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-02-25 07:18:11 +0100

    Fixes #23 and adds a Test case for it + add sample app.

--- capture-runner/src/main/groovy/com/mounacheikhna/capture/CaptureRunnerTask.groovy
+++ capture-runner/src/main/groovy/com/mounacheikhna/capture/CaptureRunnerTask.groovy
@@ -11,150 +11,180 @@ import org.gradle.process.ExecResult
  */
 public class CaptureRunnerTask extends DefaultTask implements CaptureSpec {
 
-    private String taskPrefix = ""
-    private String appApkPath
-    private String testApkPath
-    private String outputPath
-    private String serialNumber
-    private Map<String, String> instrumentationArgs
-    private String testClassName
-    private static String testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
-    private String appPackageName
-    private String testPackageName
+  private String taskPrefix = ""
+  private String appApkPath
+  private String testApkPath
+  private String outputPath
+  private String serialNumber
+  private Map<String, String> instrumentationArgs
+  private String testClassName
+  private static String testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
+  private String appPackageName
+  private String testPackageName
 
-    @TaskAction
-    public void performTask() {
-        validateInput()
-        //TODO(maybe): clean old screenshots
+  @TaskAction
+  public void performTask() {
+    //validateInput()
+    //TODO(maybe): clean old screenshots
+    //removeOldScreenshots()
+    //TODO: choose device here and set serialNumber if not specified
 
-        //TODO Validate app apk by checking that all needed permission have been asked for
-        installApks()
-        askPermissions()
-        runScreenshotsTests()
-        pullScreenshots()
+    //TODO Validate app apk by checking that all needed permission have been asked for
+    //installApks()
+    askPermissions()
+    /*runScreenshotsTests()
+    pullScreenshots()*/
 
-        //TODO open a summary of the captured screenshots ? (something that resembles spoon instead)
+    //TODO open a summary of the captured screenshots ? (something that resembles spoon instead)
+  }
+
+  def removeOldScreenshots() {
+    //from locale folder
+    if(new File(outputPath) == null || !new File(outputPath).exists()) return
+    new File(outputPath).listFiles().each {
+      it.delete()
     }
+    //from phone sdcard
 
-    void pullScreenshots() {
-        def args = ["adb", "-s", "$serialNumber", "pull", "/sdcard/app_spoon-screenshots", "$outputPath"]
-        println " pullScreenshots args : $args"
-        getProject().tasks.create("${taskPrefix}PullScreenshots", Exec) {
-            //not yet sure in which path does it get put but for now let's use spoon's
-            commandLine args
-        }.execute()
+  }
+
+  void pullScreenshots() {
+    def args = ["adb", "-s", "$serialNumber", "pull", "/sdcard/app_spoon-screenshots", "$outputPath"]
+    println " pullScreenshots args : $args"
+    getProject().tasks.create("${taskPrefix}PullScreenshots", Exec) {
+      //not yet sure in which path does it get put but for now let's use spoon's
+      commandLine args
+    }.execute()
+  }
+
+  void runScreenshotsTests() {
+    //TODO: run adb devices & check if empty or all are offline
+    def args = ["adb", "-s", "$serialNumber", "shell", "am", "instrument", "--no-window-animation"]
+    instrumentationArgs.each {
+      key, value ->
+        args.addAll("-e", key, value)
     }
+    args.addAll("-e", "class", "$testClassName", "-w",
+            "$testPackageName/$testInstrumentationRunner")
+    println " runScreenshotsTests args : $args"
+    getProject().tasks.create("${taskPrefix}RunScreenshotsTests", Exec) {
+      commandLine args
+    }.execute()
+  }
 
-    void runScreenshotsTests() {
-        def args = ["adb", "-s", "$serialNumber", "shell", "am", "instrument", "--no-window-animation"]
-        instrumentationArgs.each {
-            key, value ->
-                args.addAll("-e", key, value)
-        }
-        args.addAll("-e", "class", "$testClassName", "-w", "$testPackageName/$testInstrumentationRunner")
-        println " runScreenshotsTests args : $args"
-        getProject().tasks.create("${taskPrefix}RunScreenshotsTests", Exec) {
-            commandLine args
-        }.execute()
+  void askPermissions() {
+    String sdkVersion = getStringByCommand("adb -s $serialNumber shell getprop ro.build.version.sdk");
+    def apiLevel = Integer.parseInt(sdkVersion)
+    if(apiLevel < 23) return
+
+    def grantWriteStorage = ["adb", "-s", "$serialNumber", "shell", "pm", "grant", "$appPackageName", "android.permission.WRITE_EXTERNAL_STORAGE"]
+    getProject().tasks.create("${taskPrefix}GrantWriteStorage", Exec) {
+        commandLine grantWriteStorage
+    }.execute()
+
+    def grantReadStorage = ["adb", "-s", "$serialNumber", "shell", "pm", "grant", "$appPackageName", "android.permission.READ_EXTERNAL_STORAGE"]
+    getProject().tasks.create("${taskPrefix}GrantReadStorage", Exec) {
+      commandLine grantReadStorage
+    }.execute()
+
+  }
+
+  public static String getStringByCommand(String command) throws IOException {
+    Process process = Runtime.getRuntime().exec(command);
+    BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));
+    StringBuilder result = new StringBuilder();
+    try {
+      int value = 0;
+      while ((value = br.read()) != -1) {
+        char c = (char) value;
+        result.append(c);
+      }
+    } finally {
+      br.close();
     }
+    process.waitFor();
+    return result.toString().trim();
+  }
 
-    void askPermissions() {
-        //check if we are on >= 23 if
-        def checkApi23Args = ["adb", "-s", "$serialNumber", "shell", "getprop", "ro.build.version.sdk"]
-        ExecResult result = getProject().exec {
-            commandLine checkApi23Args
-        }
-        println "result : $result"
-        def apiLevel = result.getExitValue()
-        println "ApiLevel : $apiLevel"
+  void installApks() {
+    println "Installing app APK"
+    def appArgs = ["adb", "-s", "$serialNumber", "install", "-r", "$appApkPath"]
+    getProject().tasks.create("${taskPrefix}InstallApp", Exec) {
+      commandLine appArgs
+    }.execute()
+    //TODO: find a way to get task result and print it
+    println 'Installing tests app APK'
+    def testAppArgs = ["adb", "-s", "$serialNumber", "install", "-r", "$testApkPath"]
+    getProject().tasks.create("${taskPrefix}InstallTestApp", Exec) {
+      commandLine testAppArgs
+    }.execute()
+  }
 
-        /*def args = ["adb", "-s", "$serialNumber", "pm", "grant", "$appPackageName"]
-        println " pullScreenshots args : $args"
-        getProject().tasks.create("pullScreenshots", Exec) {
-            //not yet sure in which path does it get put but for now let's use spoon's
-            commandLine args
-        }.execute()*/
+  private void validateInput() {
+    if (!new File(appApkPath).exists()) {
+      throw new IllegalArgumentException("You must provide a valid path for app apk.")
     }
-
-    void installApks() {
-        println "Installing app APK"
-        def appArgs = ["adb", "-s", "$serialNumber", "install", "-r", "$appApkPath"]
-        getProject().tasks.create("${taskPrefix}InstallApp", Exec) {
-            commandLine appArgs
-        }.execute()
-        //TODO: find a way to get task result and print it
-        println 'Installing tests app APK'
-        def testAppArgs = ["adb", "-s", "$serialNumber", "install", "-r", "$testApkPath"]
-        getProject().tasks.create("${taskPrefix}InstallTestApp", Exec) {
-            commandLine testAppArgs
-        }.execute()
+    if (!new File(testApkPath).exists()) {
+      throw new IllegalArgumentException("You must provide a valid path for test app apk.")
     }
-
-    private void validateInput() {
-        if (!new File(appApkPath).exists()) {
-            throw new IllegalArgumentException("You must provide a valid path for app apk.")
-        }
-        if (!new File(testApkPath).exists()) {
-            throw new IllegalArgumentException("You must provide a valid path for test app apk.")
-        }
-        if (!outputPath?.trim()) {
-            throw new IllegalArgumentException("You must provide a valid output directory.")
-        } else {
-            def outputDir = new File(outputPath)
-            if (!outputDir.exists() || !outputDir.isDirectory()) {
-                throw new IllegalArgumentException("You must provide an existing directory for the output.")
-            }
-        }
-        //TODO: check if no serialNumber select the first one by default
+    if (!outputPath?.trim()) {
+      throw new IllegalArgumentException("You must provide a valid output directory.")
+    } else {
+      def outputDir = new File(outputPath)
+      if (!outputDir.exists() || !outputDir.isDirectory()) {
+        throw new IllegalArgumentException("You must provide an existing directory for the output.")
+      }
     }
+    //TODO: check if no serialNumber select the first one by default
+  }
 
-    @Override
-    void appApkPath(String appApkPath) {
-        this.appApkPath = appApkPath
-    }
+  @Override
+  void appApkPath(String appApkPath) {
+    this.appApkPath = appApkPath
+  }
 
-    @Override
-    void testApkPath(String testApkPath) {
-        this.testApkPath = testApkPath
-    }
+  @Override
+  void testApkPath(String testApkPath) {
+    this.testApkPath = testApkPath
+  }
 
-    @Override
-    void outputPath(String outputPath) {
-        this.outputPath = outputPath
-    }
+  @Override
+  void outputPath(String outputPath) {
+    this.outputPath = outputPath
+  }
 
-    @Override
-    void instrumentationArgs(Map<String, String> args) {
-        this.instrumentationArgs = args
-    }
+  @Override
+  void instrumentationArgs(Map<String, String> args) {
+    this.instrumentationArgs = args
+  }
 
-    @Override
-    void testClassName(String testClassName) {
-        this.testClassName = testClassName
-    }
+  @Override
+  void testClassName(String testClassName) {
+    this.testClassName = testClassName
+  }
 
-    @Override
-    void serialNumber(String deviceSerialNumber) {
-        this.serialNumber = deviceSerialNumber
-    }
+  @Override
+  void serialNumber(String deviceSerialNumber) {
+    this.serialNumber = deviceSerialNumber
+  }
 
-    @Override
-    void testInstrumentationRunner(String testInstrumentationRunner) {
-        this.testInstrumentationRunner = testInstrumentationRunner
-    }
+  @Override
+  void testInstrumentationRunner(String testInstrumentationRunner) {
+    this.testInstrumentationRunner = testInstrumentationRunner
+  }
 
-    @Override
-    void appPackageName(String appPackageName) {
-        this.appPackageName = appPackageName
-    }
+  @Override
+  void appPackageName(String appPackageName) {
+    this.appPackageName = appPackageName
+  }
 
-    @Override
-    void testPackageName(String testPackageName) {
-        this.testPackageName = testPackageName
-    }
+  @Override
+  void testPackageName(String testPackageName) {
+    this.testPackageName = testPackageName
+  }
 
-    @Override
-    void taskPrefix(String taskPrefix) {
-        this.taskPrefix = taskPrefix
-    }
+  @Override
+  void taskPrefix(String taskPrefix) {
+    this.taskPrefix = taskPrefix
+  }
 }
---
+++ capture-runner/src/test/groovy/com/mounacheikhna/capture/CaptureRunnerTest.groovy
@@ -0,0 +1,48 @@
+package com.mounacheikhna.capture
+
+import org.gradle.api.Project
+import org.gradle.api.Task
+import org.gradle.testfixtures.ProjectBuilder
+import org.junit.Before
+import org.junit.Test
+
+/**
+ * Created by cheikhnamouna on 2/25/16.
+ */
+class CaptureRunnerTest {
+
+  public static final String FIXTURE_WORKING_DIR = new File("src/test/fixtures/app")
+  private Project project
+
+  @Before
+  public void setUp() throws Exception {
+    project = ProjectBuilder.builder().withProjectDir(new File(FIXTURE_WORKING_DIR)).build()
+    project.apply plugin: 'com.android.application'
+    project.android {
+      compileSdkVersion 23
+      buildToolsVersion '23.0.1'
+
+      defaultConfig {
+        versionCode 1
+        versionName '1.0'
+        minSdkVersion 23
+        targetSdkVersion 23
+      }
+    }
+    project.evaluate()
+  }
+
+  @Test
+  public void testGenerateJson() {
+    Task captureTask = project.tasks.create("captureTask", CaptureRunnerTask.class)
+    captureTask.taskPrefix("test")
+    captureTask.appApkPath("build/outputs/apk/dummy.apk")
+    captureTask.testApkPath("build/outputs/apk/dummy-androidTest.apk")
+    captureTask.appPackageName("com.mounacheikhna.capture")
+    captureTask.testPackageName("com.mounacheikhna.capture.test")
+    captureTask.serialNumber("041e4f7325232e3c")
+    captureTask.testClassName("com.mounacheikhna.capture.TestClass")
+    captureTask.execute()
+  }
+
+}
